62ce89bb4bf92fa9685df71639a77b329e6da147
Maagnitude <yiwrgos_v.i.p@hotmail.com>
2023-07-31 18:17:15 +0300

    bug: it works but agent doesn't learn efficiently

--- grid_env.py
+++ grid_env.py
@@ -1,6 +1,7 @@
 import gym
 from gym import spaces
 import numpy as np
+from visualization import visualize_grid
 
 def manhattan(a, b):
     return sum(abs(val1-val2) for val1, val2 in zip(a,b))
@@ -11,11 +12,17 @@ class GridEnvironment(gym.Env):
         self.agent_value = 2
         self.obstacle_value = 1
         self.free_block_value = 0
+        self.goal_value = 3
         self.reward = 0
         self.hit_obstacle = 0
         self.closer = False
+        self.stand_still = False
+        self.moves = 0
+        
+        self.actions = ["turn_left", "turn_right", "move_forward"]
+        self.num_actions = len(self.actions)
 
-        self.action_space = spaces.Discrete(4)  # Up, Down, Left, Right
+        self.action_space = spaces.Discrete(self.num_actions)
         self.observation_space = spaces.Box(low=0, high=1, shape=(self.grid_size, self.grid_size), dtype=np.float32)
 
         self.reset()
@@ -23,10 +30,15 @@ class GridEnvironment(gym.Env):
     def reset(self):
         self.grid = np.zeros((self.grid_size, self.grid_size), dtype=np.float32)
         self.agent_pos = (0, 0)
+        self.agent_orientation = 1  # 0 for NORTH, 1 for EAST, 2 for SOUTH, 3 for WEST
         self.goal_pos = (self.grid_size - 1, self.grid_size - 1)
         self.reward = 0
+        self.stand_still = False
+        self.closer = False
+        self.moves = 0
+        self.goal_from_agent = (2, 1)
 
-        self.grid[self.goal_pos] = self.free_block_value
+        self.grid[self.goal_pos] = self.goal_value
         self.grid[self.agent_pos] = self.free_block_value
         
         # Randomly place obstacles
@@ -51,58 +63,92 @@ class GridEnvironment(gym.Env):
         return self.grid.copy()
 
     def step(self, action):
+        yaxis_orientation, xaxis_orientation = self.goal_from_agent
         x, y = self.agent_pos
         old_position = self.agent_pos
 
-        if action == 0:  # Up
-            x -= 1
-        elif action == 1:  # Down
-            x += 1
-        elif action == 2:  # Left
-            y -= 1
-        elif action == 3:  # Right
-            y += 1
+        if action == 0:  # turn_left
+            self.agent_orientation = (self.agent_orientation - 1) % 4
+            self.stand_still = True
+        elif action == 1:  # turn_right
+            self.agent_orientation = (self.agent_orientation + 1) % 4
+            self.stand_still = True
+        elif action == 2:  # move_forward
+            if self.agent_orientation == 0:  # NORTH
+                x -= 1
+            elif self.agent_orientation == 1:  # EAST
+                y += 1
+            elif self.agent_orientation == 2:  # SOUTH
+                x += 1
+            elif self.agent_orientation == 3:  # WEST
+                y -= 1   
+            self.stand_still = False  
+          
+        # OLD ACTIONS        
+        # if action == 0:  # Up
+        #     x -= 1
+        # elif action == 1:  # Down
+        #     x += 1
+        # elif action == 2:  # Left
+        #     y -= 1
+        # elif action == 3:  # Right
+        #     y += 1
 
         # Check if the new position is valid
-        if 0 <= x < self.grid_size and 0 <= y < self.grid_size and self.grid[x, y] != self.obstacle_value:
+        if 0 <= x < self.grid_size and 0 <= y < self.grid_size and self.grid[x, y] != self.obstacle_value and self.stand_still == False:
             self.grid[self.agent_pos] = self.free_block_value
             self.agent_pos = (x, y)
             self.grid[self.agent_pos] = self.agent_value
-        else:
-            self.grid[self.agent_pos] = self.free_block_value
+            if self.agent_orientation in self.goal_from_agent:
+                self.reward = 60.0
+            else:
+                self.reward = 30.0
+        elif self.stand_still == True and (self.agent_orientation in self.goal_from_agent):
+            # Agent is facing the goal
             self.grid[self.agent_pos] = self.agent_value
-            self.reward -= manhattan(self.goal_pos, self.agent_pos)
+            self.reward = -0.4
+        elif self.stand_still == True and (self.agent_orientation not in self.goal_from_agent):  
+            # Agent is not facing the goal   
+            self.grid[self.agent_pos] = self.agent_value
+            self.reward = -1.0
+        else:
+            # Agent hit an obstacle
+            self.grid[self.agent_pos] = self.agent_value
+            self.reward = -2.0
             self.hit_obstacle += 1
-
+        
+        self.moves += 1
         done = self.agent_pos == self.goal_pos
         if done:
-            self.reward += 10000000.0
+            self.reward = 1000.0
         elif manhattan(self.goal_pos, self.agent_pos) < manhattan(self.goal_pos, old_position):
             # Reward agent for moving closer to goal
             self.closer = True
-            self.reward += (30 - manhattan(self.goal_pos, self.agent_pos))
+            self.reward = (self.grid_size*2 - manhattan(self.goal_pos, self.agent_pos))
+            if self.agent_orientation == 0 or self.agent_orientation == 2:
+                yaxis_orientation = self.agent_orientation
+            if self.agent_orientation == 1 or self.agent_orientation == 3:
+                xaxis_orientation = self.agent_orientation
+            self.goal_from_agent = (yaxis_orientation, xaxis_orientation)
         elif manhattan(self.goal_pos, self.agent_pos) > manhattan(self.goal_pos, old_position):
             # Penalize agent for moving away from goal
-            self.reward -= manhattan(self.goal_pos, self.agent_pos)
+            self.reward = -(self.grid_size*2 - manhattan(self.goal_pos, self.agent_pos))
+            self.closer = False
+            if self.agent_orientation == 0 or self.agent_orientation == 2:
+                yaxis_orientation = self.agent_orientation
+            if self.agent_orientation == 1 or self.agent_orientation == 3:
+                xaxis_orientation = self.agent_orientation
+            self.goal_from_agent = (yaxis_orientation, xaxis_orientation)
+        else:
             self.closer = False
 
         return self.grid.copy(), self.reward, done, {}
 
     def render(self):
-        print(self.grid)
-
-# Example usage
-# env = GridEnvironment()
-# observation = env.reset()
-
-# done = False
-# action_list = []
-# while not done:
-#     action = env.action_space.sample()
-#     observation, reward, done, _ = env.step(action)
-#     action_list.append(action)
-#     env.render()
-
-# print(f"Action list: {action_list}")
-# print(f"Final reward: {reward}")
-# print(f"Hit obstacle {HIT_OBSTACLE} times!")
+        # print(self.grid)
+        visualize_grid(self.grid, self.agent_pos, self.agent_orientation)
+        
+if __name__ == "__main__":
+    env = GridEnvironment()
+    env.reset()
+    env.render()
--- model_load.py
+++ model_load.py
@@ -2,6 +2,7 @@ from stable_baselines3 import PPO
 from grid_env import GridEnvironment
 import os
 from datetime import datetime
+import pygame
 
 # Create the environment
 env = GridEnvironment()
@@ -13,30 +14,32 @@ model_path = f"{models_dir}/{model_name}"
 # Load the trained agent
 model = PPO.load(model_path, env=env)
 
-episodes = 10
+episodes = 5
 total_rewards = 0
 
 for ep in range(episodes):
     obs = env.reset()
     done = False
-    episode_reward = 0
-    
+    episode_reward = 0   
     
     done = False
-    action_list = []
     
     while not done:
         action, _ = model.predict(obs)
         obs, reward, done, info = env.step(action)
         episode_reward += reward
-        action_list.append(action)
         env.render()
+        pygame.time.delay(30)  # Adjust the speed of visualization (30 milliseconds)
         
-        # print(f"Action list: {action_list}")
-        print(f"Action taken: {action}")
-        print(f"Agent moved " + ("closer" if env.closer else "away") + " from the goal!")
+        # INFO
+        print(f"Total moves: {env.moves}")
+        print(f"Action taken: {env.actions[action]}")
+        if env.stand_still == False:
+            print(f"Agent moved " + ("closer to" if env.closer else "away from") + " the goal!")
+        else:
+            print(f"Agent stood still!")
         print(f"Episode reward: {episode_reward}")
-        print(f"Hit obstacle {env.hit_obstacle} times!")
+        print(f"Hit obstacle {env.hit_obstacle} times!\n")
         
     total_rewards += episode_reward
 
@@ -54,3 +57,6 @@ with open(file_path, "a") as f:
 
 # Save the trained agent
 model.save(models_dir + f"/{model_name}_new")
+
+# Close the Pygame window when the script ends
+pygame.quit()
