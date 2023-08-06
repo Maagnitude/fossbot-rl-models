41b4a99e9359b310eabb8e00e81757503e95a7a2
Maagnitude <yiwrgos_v.i.p@hotmail.com>
2023-08-06 17:59:39 +0300

    feat: gifs for 15x15 trpo and 30x30 dqn

--- gif_maker.py
+++ gif_maker.py
@@ -2,11 +2,11 @@ import pygame
 from visualization import win_size
 import imageio
 import numpy as np
-from stable_baselines3 import DQN
+from sb3_contrib import TRPO
 from grid_env import GridEnvironment
 
 env = GridEnvironment()
-model = DQN.load("models/DQN/dqn_gridworld_15x15", env=env)
+model = TRPO.load("models/TRPO/trpo_gridworld_15x15", env=env)
 
 images = []
 obs = model.env.reset()
@@ -15,7 +15,8 @@ pygame.init()
 screen = pygame.display.set_mode((win_size[0], win_size[1]))
 env.render()
 
-for i in range(20):
+for i in range(25):
+    reward = 0
     
     # Capture the pygame screen as an image
     img = pygame.surfarray.array3d(screen)
@@ -25,7 +26,16 @@ for i in range(20):
     images.append(img)
     
     action, _ = model.predict(obs)
-    obs, _, _, _ = env.step(action)
-    env.render()
+    obs, reward, _, _ = env.step(action)
+    
+    if reward == 1000.0:
+        env.render()
+        img = pygame.surfarray.array3d(screen)
+    
+        img = np.flipud(np.rot90(img, k=1))
+        images.append(img)
+        break
+    else:
+        env.render()
 
-imageio.mimsave("gifs/dqn_gridworld_15x15.gif", [np.array(img) for i, img in enumerate(images)], duration=90, loop=0)
+imageio.mimsave("gifs/trpo_gridworld_15x15.gif", [np.array(img) for i, img in enumerate(images)], duration=90, loop=0)
